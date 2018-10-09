c0a3befdf0359178e35fb8f9f35a5db5f7186009
Devon <dev@knowncreative.co>
2018-10-09 07:52:46 -0400

    Progress!

--- src/canvas.js
+++ src/canvas.js
@@ -1,3 +1,6 @@
+import './../node_modules/svg.js/dist/svg.js';
+import './../node_modules/svg.filter.js/dist/svg.filter.js';
+
 var canvas = document.getElementById("animation");
 var canvasW = window.innerWidth;
 var canvasH = window.innerHeight;
@@ -9,6 +12,7 @@ var progress = 0;
 
 var svg = canvas.querySelector('svg');
 var svgBbox = svg.getBBox();
+var svgObj = SVG.adopt(svg);
 svg.style.position = 'absolute';
 svg.style.left = (canvasW / 2) - (svgBbox.width / 2) + 'px';
 svg.style.top = (canvasH / 2) - (svgBbox.height / 2) + 'px';
@@ -19,39 +23,47 @@ var paths = svg.querySelectorAll('path');
 
 for (var i = 0; i < paths.length; i++) {
     var p = paths[i];
-    var boundingRect = p.getBoundingClientRect();
 
     p.style.position = 'relative';
     p.style.transformOrigin = 'center center';
+    p.style.zIndex = paths.length - i;
 
     pathsArr.push({
         ref: p,
-        startFrame: i * 50,
+        svgObj: svgObj.children()[i],
+        startFrame: i * 60,
         scale: 1,
+        duration: 420,
+        opacity: 1,
         isAnimating: true,
         endFrame: 0,
-        direction: Math.random() < 0.5 ? -1 : 1
+        direction: 1
+        //direction: Math.random() < 0.5 ? -1 : 1
     });
 }
 
-console.log(pathsArr);
 
-window.addEventListener('mousewheel', mouseScroll);
 
-function mouseScroll(e) {
-    e.preventDefault();
-    var deltaY = e.deltaY;
+// window.addEventListener('mousewheel', mouseScroll);
+//
+// function mouseScroll(e) {
+//     e.preventDefault();
+//     var deltaY = e.deltaY;
+//     console.log(deltaY);
+//     render(deltaY);
+// }
 
-    render(deltaY);
-}
+render();
 
-function isShapeOnScreen(position) {
-    return (position.bottom <= 0 || position.top >= window.innerHeight || position.left >= window.innerWidth || position.right <= 0) ? false : true;
-}
+
+
+// function isShapeOnScreen(position) {
+//     return (position.bottom <= 0 || position.top >= window.innerHeight || position.left >= window.innerWidth || position.right <= 0) ? false : true;
+// }
 
 function render(delta) {
-
-    var zoomDirection = (delta < 0) ? 'in' : 'out';
+    //var zoomDirection = (delta < 0) ? 'in' : 'out';
+    var zoomDirection = 'in';
     var i = pathsArr.length;
 
     progress += (zoomDirection == 'in') ? 1 : -1;
@@ -61,234 +73,69 @@ function render(delta) {
         return;
     }
 
-    //console.log(progress);
-
     while(i--) {
         var path = pathsArr[i];
+        var startFrame = path.startFrame;
+        var duration = path.duration;
 
-        // if(progress < startFrame) {
-        //     continue;
-        // } else {
-        //     path.isAnimating = true;
-        // }
+        // Set shape to animating and visible when reaching it's endFrame in reverse
+        if(path.endFrame > progress && path.isAnimating == false) {
+            path.isAnimating = true;
+            path.ref.style.display = 'block';
+        }
 
-        if(path.isAnimating && (progress >= path.startFrame)) {
-            var startFrame = path.startFrame;
-            var dir = path.direction;
-            var position = path.ref.getBoundingClientRect();
-            var shapeProgress = (progress - startFrame) * path.direction; // time since this shape started animating
-            var increment = 0;
+        // Move to next shape if this one is not animating
+        if(!path.isAnimating) continue;
 
-            var scale = fov / (fov - (shapeProgress * 5));
+        //console.log(progress);
 
-            //console.log(scale);
+        // z-indexing
+        if(path.startFrame == progress) {
+            (path.direction > 0) ? svg.appendChild(path.ref) : svg.insertBefore(svg.children[0], path.ref);
+        }
 
-            if(!isFinite(scale)) {
+        // Check if we've hit shape start
+        if(progress >= path.startFrame) {
+
+            // time since this shape started animating
+            var shapeProgress = (progress - startFrame) * path.direction;
+
+            // % through animation
+            //var scale = fov / (fov - (shapeProgress));
+            var scale = shapeProgress / duration;
+
+            // New opacity
+            var newOpacity = 1 - scale;
+
+            // Shape is finished animating - new scale value == infinity
+            // Mark endFrame to track when to resume animation in reverse
+            //if(!isFinite(scale)) {
+            if(shapeProgress == duration) {
                 path.isAnimating = false;
                 path.endFrame = progress;
+                path.ref.style.display = 'none';
                 continue;
             }
 
-            path.ref.style.transform = 'scale(' + scale + ')';
+            // Adjust scale
+            path.ref.style.transform = 'scale(' + (scale + 1) + ')';
 
-            //console.log(path.z, fov, (fov + path.z), scale);
-
-            // if (path.z >= path.start.z) {
-            //     path.z = path.start.z;
-            // }
-
-        } else {
-            var something = progress - path.endFrame;
-            if(something == 0) {
-                path.isAnimating = true;
+            // Adjust opacity when moving into background
+            path.svgObj.attr('fill-opacity', newOpacity);
+            if(scale > (duration / 60) / 1.1) {
+                path.svgObj.attr('fill-opacity', newOpacity);
             }
+
+            // Adjust blur
+            path.svgObj.filter(function(add) {
+                add.gaussianBlur(scale * 3);
+            });
+
         }
+    }
 
-        // Check if pixel should being animating yet
-        //if(progress < path.startFrame) continue;
-
-        // Increase z while pixel is on screen
-        // if (path.offscreenDepth == 0) {
-        //     path.z += (zoomDirection == 'in') ? -5 : 5;
-        //     progress += (zoomDirection == 'in') ? -1 : 1;
-        //     if (path.z > path.start.z) {
-        //         path.z = path.start.z;
-        //     }
-        // }
-
-        // var radians = Math.atan2(svgBbox.height / 2 - (path.bbox.height / 2 + path.bbox.y), svgBbox.width / 2 - (path.bbox.width / 2 + path.bbox.x));
-        // var angle = radians * 180 / Math.PI;
-        // var a = svgBbox.height / 2 - (bbox.height / 2 - bbox.y / 2);
-        // var b = svgBbox.width / 2 - (bbox.width / 2 - bbox.x / 2);
-        // var c = Math.sqrt(a * a + b * b);
-        // console.log('radians: ' + radians + '\nangle: ' + angle);
-        // console.log('x: ' + -(speed * Math.sin(angle)));
-        // console.log('y: ' + -(speed * Math.cos(angle)));
-        // console.log('c: '+c);
-
-
-        // var w = path.w * scale;
-        // var h = path.h * scale;
-        // var x2d = (path.x * scale) + canvasW / 2;
-        // var y2d = (path.y * scale) + canvasH / 2;
-
-        // If pixel is off screen
-        //if(pixel.z == fov - fov*2) {
-        // if (!pixelOnScreen(x2d, y2d, w, h)) {
-        //
-        //     path.offscreenDepth += (zoomDirection == 'in') ? 1 : -1;
-        //
-        //     // Pixel depth should never go below 0: the start position
-        //     if (path.offscreenDepth < 0) {
-        //         path.offscreenDepth = 0;
-        //     }
-        //
-        //     if (path.offscreenDepth != 0) continue;
-        //
-        // } else {
-        //
-        //     if (!pixelOnScreen(x2d, y2d, w, h)) continue;
-        //
-        //     path.animationEnd = false;
-        // }
+    if(progress < 800) {
+        window.requestAnimationFrame(render);
     }
 
 }
-
-
-// var canvas = document.querySelector("canvas");
-// var canvasW = canvas.width = window.innerWidth;
-// var canvasH = canvas.height = window.innerHeight;
-// var fov = 100; //pixels are 250px away from us
-// var progress = 0;
-// var ctx = canvas.getContext("2d");
-// canvas.style.backgroundColor = 'black';
-//
-// var pixels = [
-//     {
-//         start: {w: 10, h: 10, z: 0},
-//         x: -20,
-//         y: -20,
-//         z: 0,
-//         offscreenDepth: 0,
-//         startFrame: 0,
-//         duration: 10
-//     },
-//     {
-//         start: {w: 10, h: 10, z: 0},
-//         x: 100,
-//         y: 20,
-//         z: 0,
-//         offscreenDepth: 0,
-//         startFrame: 0,
-//         duration: 10
-//     },
-//     {
-//         start: {w: 5, h: 5, z: 0},
-//         x: -30,
-//         y: 40,
-//         z: -20,
-//         offscreenDepth: 0,
-//         startFrame: 0,
-//         duration: 10
-//     },
-//     {
-//         start: {w: 10, h: 10, z: 0},
-//         x: 30,
-//         y: -40,
-//         z: -20,
-//         offscreenDepth: 0,
-//         startFrame: 0,
-//         duration: 10
-//     },
-//     {
-//         start: {w: 5, h: 5, z: 0},
-//         x: -50,
-//         y: -40,
-//         z: 0,
-//         offscreenDepth: 0,
-//         startFrame: 0,
-//         duration: 10
-//     }
-// ];
-//
-// window.addEventListener('mousewheel', mouseScroll);
-// function mouseScroll(e) {
-//     e.preventDefault();
-//     var deltaY = e.deltaY;
-//
-//     render(deltaY);
-// }
-//
-// function pixelOnScreen(x, y, w, h) {
-//     return (x > 0 - w && x + w < canvasW + w && y > 0 - h && y + h < canvasH + h) ? true : false;
-// }
-//
-// //time to draw the pixels
-// function render(delta)
-// {
-//     ctx.clearRect(0,0,canvasW,canvasH);
-//
-//     //looping through all pixel points
-//     var i = pixels.length;
-//     var incrementer = 1;
-//     while(i--) {
-//         var pixel = pixels[i];
-//         var zoomDirection = (delta < 0) ? 'in' : 'out';
-//
-//         // Check if pixel should being animating yet
-//         if(progress < pixel.startFrame) continue;
-//
-//         // Increase z while pixel is on screen
-//         if(pixel.offscreenDepth == 0) {
-//             pixel.z += (zoomDirection == 'in') ? -5 : 5;
-//             progress += (zoomDirection == ' in') ? -1 : 1;
-//             if(pixel.z > pixel.start.z) {
-//                 pixel.z = pixel.start.z;
-//             }
-//         }
-//
-//         var scale = fov/(fov+pixel.z);
-//         var w = pixel.start.w * scale;
-//         var h = pixel.start.h * scale;
-//         var x2d = (pixel.x * scale) + canvasW/2;
-//         var y2d = (pixel.y * scale) + canvasH/2;
-//
-//         // If pixel is off screen
-//         //if(pixel.z == fov - fov*2) {
-//         if(!pixelOnScreen(x2d, y2d, w, h)) {
-//
-//             pixel.offscreenDepth += (zoomDirection == 'in') ? 1 : -1;
-//
-//             // Pixel depth should never go below 0: the start position
-//             if(pixel.offscreenDepth < 0) {
-//                 pixel.offscreenDepth = 0;
-//             }
-//
-//             if(pixel.offscreenDepth != 0) continue;
-//
-//         } else {
-//
-//             if(!pixelOnScreen(x2d, y2d, w, h)) continue;
-//
-//             // Skip to next pixel - this one is done animating
-//             //if(pixel.animationEnd && delta < 0) continue;
-//
-//             pixel.animationEnd = false;
-//             ctx.beginPath();
-//             ctx.rect(x2d, y2d, w, h);
-//             ctx.fillStyle = 'white';
-//             ctx.fill();
-//             ctx.closePath();
-//         }
-//     }
-//
-//     ctx.beginPath();
-//     ctx.fillStyle = 'yellow';
-//     ctx.rect(canvasW/2 - 2, canvasH/2 - 2, 4, 4);
-//     ctx.fill();
-//     ctx.closePath();
-//
-// }
-//
-// render(false);
